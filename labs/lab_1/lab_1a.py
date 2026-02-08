d08f754367a474e6f5dbe50237513eeda4580fc9
r2d2kion <155199164+r2d2kion@users.noreply.github.com>
2026-02-08 17:23:40 -0500

    Update lab_1a.py
    Test commit

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,6 +1,6 @@
 """
 lab_1a.py
-
+yayayyayyayyay[python]
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
@@ -8,9 +8,10 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "" # TODO: Insert your name between the double quotes
+    name = "Krishna Kamal" # TODO: Insert your name between the double quotes
+    class_name = " I see your a JPS freshman" # TODO: Insert the name of the class between the double quotes
 
-    print(f"{name}, Welcome to the CSS course!")
+    print(f"{name} {class_name}, Welcome to the CSS course!")
 
 if __name__ == "__main__":
     main()
